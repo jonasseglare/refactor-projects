666b56b07c28aee1518066ab5936fc0502f29891
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-09-20 09:33:21 +0200

    good

--- src/refactor_projects/core.clj
+++ src/refactor_projects/core.clj
@@ -5,6 +5,28 @@
             [clojure.edn :as edn]
             [clojure.spec.alpha :as spec]))
 
+(spec/def ::paths (spec/coll-of string?))
+
+(spec/def ::rename (spec/cat :prefix #{:rename}
+                             :from-ns string?
+                             :to-ns string?))
+
+(spec/def ::op (spec/alt :rename ::rename))
+(spec/def ::ops (spec/coll-of ::op))
+(spec/def ::mode #{:preview :execute})
+
+(spec/def ::refactor-spec (spec/keys :req-un [::paths
+                                              ::ops
+                                              ::mode]))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;  Implementation
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 (spec/def ::clj-file (spec/alt :src-file (spec/cat :ns (partial = 'ns)
                                                    :namespace symbol?
                                                    :rest (spec/* any?))
@@ -46,6 +68,14 @@
 (defn clojure-file? [file]
   {:pre [(file? file)]}
   (.endsWith (cljstr/lower-case (str file)) ".clj"))
+
+(defn file-contains-subpath? [file p]
+  {:pre [(or (instance? File file)
+             (nil? file))]}
+  (cond
+    (nil? file) false
+    (= p (.getName file)) true
+    :default (file-contains-subpath? (.getParentFile file) p)))
 
 (defn tokenize [s]
   {:pre [(string? s)]}
@@ -93,12 +123,16 @@
                                             :leiningen-project :name)])))))))
 
 (defn analyze-clojure-file [file]
-  (let [data (slurp file)
-        edn-data (read-string data)]
-    (merge {:data data
-            :edn-data edn-data
-            :file file}
-           (parse-edn edn-data))))
+  (try
+    (let [data (slurp file)
+          edn-data (read-string data)]
+      (merge {:data data
+              :edn-data edn-data
+              :file file}
+             (parse-edn edn-data)))
+    (catch Exception e
+      (println "  ERROR: Failed to analyze" (.getAbsolutePath file))
+      {})))
 
 (defn append-test [s]
   (str s "-test"))
@@ -125,8 +159,6 @@
         lc-file (-> file
                     :file
                     .getAbsolutePath)]
-    (println "  from-ns=" (:from-ns settings))
-    (println "  parts=" parts)
     (when-let [f (find-subvec (:from-ns settings) parts)]
       (let [expected-suffix (parts-to-filename-suffix parts)]
         (if (cljstr/ends-with? lc-file expected-suffix)
@@ -139,17 +171,48 @@
 (defn perform-renaming [file rename-settings]
   (let [test-settings (:test rename-settings)
         src-settings (:src rename-settings)
-        new-data (compute-new-data (:data file) src-settings)
+        old-data (:data file)
+        old-file (:file file)
+        new-data (compute-new-data old-data src-settings)
         new-file (or (compute-new-file file test-settings)
-                     (compute-new-file file src-settings))]
-    (when (not= new-data (:data file))
-      (println "NEW DATA!!!")
-      #_(println "'" new-data "'"))
-    (println "new-file=" new-file)))
+                     (compute-new-file file src-settings))
+        updated-data? (not= new-data old-data)
+        updated-file? (boolean new-file)
+        updated? (or updated-data? updated-file?)]
+    (when updated?
+      (println (str "\n -> Update '" (.getAbsolutePath old-file) "'"))
+      (when updated-data?
+        (println "     - new data"))
+      (when updated-file?
+        (println "     - new name: " (.getAbsolutePath new-file))))
+    (case (:mode rename-settings)
+      :preview nil
+      :execute (when updated?
+                 (println "  -- Execute! --")
+                 (when new-file
+                   (io/make-parents new-file))
+                 (cond
+                   (and updated-data? updated-file?) (do (io/delete-file old-file)
+                                                         (spit new-file new-data))
+                   updated-data? (spit old-file new-data)
+                   updated-file? (.renameTo old-file new-file))))))
 
 (defn rename-namespace-for-file [file rename-settings]
   (and (= (:type file) :src-file)
        (perform-renaming file rename-settings)))
+
+(defn empty-directories [paths]
+  (transduce
+   (comp  (map (comp file-seq io/file))
+          cat
+          (filter #(and (not (file-contains-subpath? % "checkouts"))
+                        (not (file-contains-subpath? % ".git"))
+                        (.isDirectory %)
+                        (< 3 (count (file-seq %))))))
+   conj
+   []
+   paths))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -162,18 +225,48 @@
   (transduce
    (comp (map (comp file-seq io/file))
          cat
-         (filter clojure-file?)
+         (filter #(and (clojure-file? %)
+                       (not (file-contains-subpath? % "checkouts"))))
          (map analyze-clojure-file))
    conj
    []
    directories-to-scan))
 
-(defn rename-namespace [clojure-files from-ns to-ns]
+(defn rename-namespace [refactor-spec clojure-files from-ns to-ns]
   (let [basic-rename-settings {:from-ns from-ns
                                :to-ns to-ns}
         test-rename-settings (rename-settings-with-test basic-rename-settings)
-        full-rename-settings {:src (rename-settings-to-parts basic-rename-settings)
-                              :test (rename-settings-to-parts test-rename-settings)}]
+        full-rename-settings (merge refactor-spec
+                                    {:src (rename-settings-to-parts basic-rename-settings)
+                                     :test (rename-settings-to-parts test-rename-settings)})]
     (doseq [file clojure-files]
-      (println "\n* Rename namespace for" (-> file :file .getAbsolutePath))
       (rename-namespace-for-file file full-rename-settings))))
+
+(defn refactor [refactor-spec]
+  {:pre [(spec/valid? ::refactor-spec refactor-spec)]}
+  (doseq [op (:ops refactor-spec)]
+    (println "-------> Performing op" op)
+    (let [files (clojure-files (:paths refactor-spec))]
+      (println "  " (count files) " clojure files")
+      (let [[op-type op-data] (spec/conform ::op op)]
+        (case op-type
+          :rename (rename-namespace refactor-spec
+                                    files
+                                    (:from-ns op-data)
+                                    (:to-ns op-data)))))))
+
+
+
+#_ (refactor {:paths ["/home/jonas/prog/clojure"]
+           :ops [[:rename "bluebell.utils.wip" "bluebell.utils.wip.wip"]]
+              :mode :preview})
+
+#_(refactor {:paths ["/home/jonas/prog/clojure"]
+           :ops [[:rename "bluebell.utils.dsl" "bluebell.utils.dsl"]
+                 [:rename "bluebell.utils.render-text" "bluebell.utils.render-text"]
+                 [:rename "bluebell.utils.ebmd" "bluebell.utils.ebmd"]]
+           :mode :execute})
+
+#_(refactor {:paths ["/home/jonas/prog/clojure/test-project"]
+           :ops [[:rename "test-project" "kattskit.wip"]]
+           :mode :preview})
