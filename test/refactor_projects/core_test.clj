1cc14f36d6ed4045ab7d362645f457330c065c1d
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-09-20 09:33:29 +0200

    good

---
+++ test/refactor_projects/core_test.clj
@@ -0,0 +1,7 @@
+(ns refactor-projects.core-test
+  (:require [clojure.test :refer :all]
+            [refactor-projects.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
